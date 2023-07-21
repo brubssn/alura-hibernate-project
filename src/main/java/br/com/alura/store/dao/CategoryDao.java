7cf7035c95d6cb8f795e640f22bff853523d2783
Bruna Bassani <117603226+brubssn@users.noreply.github.com>
2023-07-21 17:19:07 -0300

    Commit do projeto teste de Hibernate pela Alura

--- src/main/java/br/com/alura/store/dao/CategoryDao.java
+++ src/main/java/br/com/alura/store/dao/CategoryDao.java
@@ -1,18 +1,37 @@
 package br.com.alura.store.dao;
 
 import br.com.alura.store.model.Category;
-import br.com.alura.store.model.Product;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class CategoryDao {
     private EntityManager em;
 
-    public CategoryDao(EntityManager em) {
+    public CategoryDao(EntityManager em){
         this.em = em;
     }
-
     public void add(Category category){
         this.em.persist(category);
+    }
+    public void update(Category category){
+        this.em.merge(category);
+    }
+    public void delete(Category category){
+        em.merge(category);
+        this.em.remove(category);
+    }
+    public Category searchById(Long id){
+        return em.find(Category.class, id);
+    }
+    public List<Category> searchAll(){
+        String jpql = "SELECT c FROM Category c";
+        return em.createQuery(jpql, Category.class).getResultList();
+    }
+    public List<Category> searchByName(String name){
+        String jpql = "SELECT c FROM Category c WHERE c.name = :name";
+        return em.createQuery(jpql, Category.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
--- src/main/java/br/com/alura/store/dao/ProductDao.java
+++ src/main/java/br/com/alura/store/dao/ProductDao.java
@@ -3,15 +3,48 @@ package br.com.alura.store.dao;
 import br.com.alura.store.model.Product;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProductDao {
     private EntityManager em;
 
-    public ProductDao(EntityManager em) {
+    public ProductDao(EntityManager em){
         this.em = em;
     }
-
     public void add(Product product){
         this.em.persist(product);
+    }
+    public void update(Product product){
+        this.em.merge(product);
+    }
+    public void delete(Product product){
+        em.merge(product);
+        this.em.remove(product);
+    }
+    public Product searchById(Long id){
+        return em.find(Product.class, id);
+    }
+    public List<Product> searchAll(){
+        String jpql = "SELECT p FROM Product p";
+        return em.createQuery(jpql, Product.class).getResultList();
+    }
+    public List<Product> searchByName(String name){
+        String jpql = "SELECT p FROM Product p WHERE p.name = :name";
+        return em.createQuery(jpql, Product.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+    public List<Product> searchByCategory(String name){
+        String jpql = "SELECT p FROM Product p WHERE p.category.name = :name";
+        return em.createQuery(jpql, Product.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+    public BigDecimal searchPriceByName(String name){
+        String jpql = "SELECT p.price FROM Product p WHERE p.name = :name";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("name", name)
+                .getSingleResult();
     }
 }
